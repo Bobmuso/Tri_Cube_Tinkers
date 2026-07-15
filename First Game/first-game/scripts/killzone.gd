5c9f6e239b948bf82a7eb383e1281101a33101bc
Bobmuso <nyembob2@gmail.com>
2026-07-15 20:44:23 +0200

    slimed

--- First Game/first-game/scripts/killzone.gd
+++ First Game/first-game/scripts/killzone.gd
@@ -4,9 +4,12 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died") # Replace with function body.
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene() # Replace with function body.
